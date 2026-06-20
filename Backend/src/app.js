22fdbe051246f993264824d19e821fc1413666e2
Shiva181006 <shivakasaudhan817@gmail.com>
2026-06-20 19:58:48 +0530

    Fix production cookie settings

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,9 +7,12 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://hire-lens-ai-liart.vercel.app"
+    ],
     credentials: true
-}))
+}));
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
